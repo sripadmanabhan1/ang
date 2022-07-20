729c1c8fa9651219e9f7cc36477c90f90da52a0d
sripadmanabhan1 <106224413+sripadmanabhan1@users.noreply.github.com>
2022-07-20 17:59:16 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Valid
+# Event
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.4.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/valid'),
+      dir: require('path').join(__dirname, './coverage/event'),
       subdir: '.',
       reporters: [
         { type: 'html' },
